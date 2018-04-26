c0b8b0b63e6c0647f725e2ac5e946a793147062e
stephbeaird <sbeaird22@gmail.com>
2018-04-26 14:52:17 -0700

    change limit in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @orders = ShopifyAPI::Order.find(:all, params: { limit: 15 })
+    @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
   end
 end
