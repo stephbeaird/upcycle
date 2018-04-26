657b1be1a10314ec6ddc5e6653cc7fa717122e09
stephbeaird <sbeaird22@gmail.com>
2018-04-26 11:33:42 -0700

    orders to products

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @orders = ShopifyAPI::Product.find(:all, params: { limit: 15 })
+    @products = ShopifyAPI::Product.find(:all, params: { limit: 15 })
   end
 end
