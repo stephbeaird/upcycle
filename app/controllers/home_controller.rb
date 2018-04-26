ba46a3b6d2b7c3db97ca1589caee4e02eee98ba6
stephbeaird <sbeaird22@gmail.com>
2018-04-26 12:43:01 -0700

    changes to search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 15 })
-    if params[:search]
-      @orders = Order.search(params[:search]).number("created_at DESC")
-    else
-      @orders = Order.all.number("created_at DESC")
+    # if params[:search]
+    #   @orders = Order.search(params[:search]).number("created_at DESC")
+    # else
+    #   @orders = Order.all.number("created_at DESC")
   end
 end
