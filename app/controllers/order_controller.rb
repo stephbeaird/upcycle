cc9774d7c7bb03c7e1e39090a5e06c9e207fab0f
stephbeaird <sbeaird22@gmail.com>
2018-04-26 16:23:37 -0700

    reverted all orders controllers and models

--- app/controllers/order_controller.rb
+++
@@ -1,7 +0,0 @@
-class OrderController < ShopifyApp::AuthenticatedController
-  if params[:number]
-    @orders = Order.search_by_order_number(params[:number])
-  else
-    @orders = Order.all
-  end
-end
--- app/models/order.rb
+++
@@ -1,6 +0,0 @@
-class Order < ActiveRecord::Base
-  include ShopifyApp::SessionStorage
-  include PgSearch
-  pg_search_scope :search_by_order_number, against: [:order, :number]
-end
-  
