4bd56737f7e4111e21b627a2dcac400db44ff8e9
stephbeaird <sbeaird22@gmail.com>
2018-04-26 15:13:20 -0700

    added order query in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
+    if params[:search]
+      @orders = Order.search(params[:search]).number("created_at DESC")
+    else
+      @orders = Order.all.number("created_at DESC")
   end
 end
