a4fb7c15196d8a7857c37197f09e44ec795617f2
stephbeaird <sbeaird22@gmail.com>
2018-04-27 16:16:08 -0700

    got rid of price_rules

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
-    @price_rules = Price_Rules.all
   end
 end
