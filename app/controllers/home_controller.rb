5132cad58db1adf9fff2692a479ebfe99f27ed69
stephbeaird <sbeaird22@gmail.com>
2018-04-27 15:59:18 -0700

    removed order controller. added s to rules in index.html

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
-    @price_rules = Price_Rule.find(:all)
+    @price_rules = Price_Rules.find(:all)
   end
 end
