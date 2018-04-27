ece1912060424a79467fd2c2cddaca97f779e27d
stephbeaird <sbeaird22@gmail.com>
2018-04-27 15:40:24 -0700

    added @price_rule under @orders to views/index and controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
+    @price_rule = ShopifyAPI::Price_Rule.find(:all, params: { limit: 25 })
   end
 end
