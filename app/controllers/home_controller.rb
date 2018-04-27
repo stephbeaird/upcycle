94664463d1f62c398de108813fc17d0b86f79aad
stephbeaird <sbeaird22@gmail.com>
2018-04-27 12:56:04 -0700

    added params to home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
+    params.require(:task).permit(:order.number)
   end
 end
