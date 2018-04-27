3a47950b7048a2dff5955da2015836fe2bb100cc
stephbeaird <sbeaird22@gmail.com>
2018-04-27 15:48:10 -0700

    removed params from pr controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
-    @pricerules = ShopifyAPI::PriceRule.find(:all, params: { limit: 25 })
+    @pricerules = ShopifyAPI::PriceRule.find(:all)
   end
 end
