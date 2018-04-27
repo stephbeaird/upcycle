a63d767d22e2cdad34f5d96f6636b8e8d1c0ded1
stephbeaird <sbeaird22@gmail.com>
2018-04-27 15:42:41 -0700

    from price_rule to pricerules

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
-    @price_rule = ShopifyAPI::Price_Rule.find(:all, params: { limit: 25 })
+    @pricerules = ShopifyAPI::PriceRule.find(:all, params: { limit: 25 })
   end
 end
