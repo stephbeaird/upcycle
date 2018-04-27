9c7147b793fcf26774f496435252764c243ee654
stephbeaird <sbeaird22@gmail.com>
2018-04-27 10:36:31 -0700

    removed if/else statement from search in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,5 @@ class HomeController < ShopifyApp::AuthenticatedController
   end
 
   def search
-    if params[:search].blank?
-      redirect_to(root_path, alert: "Empty field!") and return
-    else
-      @parameter = params[:search].downcase
-      @results = Store.all.where("order(number) LIKE :search", search: @parameter)
   end
 end
