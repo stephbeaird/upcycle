7a4003fcf20617ef2273181715695d739ba2b96b
stephbeaird <sbeaird22@gmail.com>
2018-04-27 15:51:03 -0700

    removed @pr in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
-    @pricerules = ShopifyAPI::PriceRule.find(:all)
   end
 end
