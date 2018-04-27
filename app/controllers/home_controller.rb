7949e1fb1a609375ff015ef6b026e5a6a87d535e
stephbeaird <sbeaird22@gmail.com>
2018-04-27 14:16:39 -0700

    removed form tag from index/html/erb and removed params in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
-    params.require(:task).permit(:order.number)
   end
 end
