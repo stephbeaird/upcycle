4df81ab39b1ace4323d5d4ee15e81e102d5ae94e
stephbeaird <sbeaird22@gmail.com>
2018-04-30 11:51:04 -0700

    added "button" method in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,8 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
   end
+
+  def button
+    puts "code goes here"
+  end
 end
