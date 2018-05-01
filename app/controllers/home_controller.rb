5029646bf1a1d42922d943a7f403200184c4fbc6
stephbeaird <sbeaird22@gmail.com>
2018-05-01 15:38:17 -0700

    changed find to find_by in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ShopifyApp::AuthenticatedController
   end
 
   def search
-    @orders = ShopifyAPI::Order.find(:all, params[:number])
+    @orders = ShopifyAPI::Order.find_by(params[:number])
   end
 end
