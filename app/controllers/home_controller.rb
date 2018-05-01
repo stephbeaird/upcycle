7ed2c699f4b1b07176adf64cec0aba90fd1de453
stephbeaird <sbeaird22@gmail.com>
2018-05-01 16:21:21 -0700

    remove if else statement from controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
-    if params[:search]
-      @orders = ShopifyAPI::Order.search(params[:search]).number("created_at DESC")
-    else
-      @orders = ShopifyAPI::Order.all.number('created_at DESC')
-    end
   end
 
   def search
