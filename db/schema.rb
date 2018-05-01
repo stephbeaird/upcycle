2a988b70cda788d3e440d7f1c4eb770edd8bc633
stephbeaird <sbeaird22@gmail.com>
2018-05-01 10:53:21 -0700

    created two models, added a gem in Gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424232537) do
+ActiveRecord::Schema.define(version: 20180501175005) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "shops", force: :cascade do |t|
     t.string "shopify_domain", null: false
