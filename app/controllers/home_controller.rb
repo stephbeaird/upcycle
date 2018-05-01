476df9d412023123ffbdc151b5523f20479b67e2
stephbeaird <sbeaird22@gmail.com>
2018-05-01 10:58:23 -0700

    removed code from controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
-    @search = Search.new(Order, params[:search])
-    @orders = @order.run.paginate(page: params[:number])
   end
 end
