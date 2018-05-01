bf0a57604c64eda01981758495b0bac59d3797d0
stephbeaird <sbeaird22@gmail.com>
2018-05-01 16:42:07 -0700

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   mount ShopifyApp::Engine, at: '/'
 
   get '/search' => 'home#search'
-  get '/upcycle' => 'upcycles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
