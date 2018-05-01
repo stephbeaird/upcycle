22af29211f27b01d1e2b4c2cc543ad783277412d
stephbeaird <sbeaird22@gmail.com>
2018-05-01 16:36:19 -0700

    form_tag added to search.html

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   mount ShopifyApp::Engine, at: '/'
 
   get '/search' => 'home#search'
+  get '/upcycle' => 'upcycles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
