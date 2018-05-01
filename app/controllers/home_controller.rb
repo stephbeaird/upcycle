90a26cd56f21d0963e5694dceaf8b735ad8f7f54
stephbeaird <sbeaird22@gmail.com>
2018-05-01 10:56:39 -0700

    added code to controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
+    @search = Search.new(Order, params[:search])
+    @orders = @order.run.paginate(page: params[:number])
   end
 end
