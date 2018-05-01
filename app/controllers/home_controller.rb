45912738581efc07b4c1075d510b0124cae0b030
stephbeaird <sbeaird22@gmail.com>
2018-04-30 17:20:25 -0700

    if else statement out of controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
-    if params[:search]
-      @orders = Order.search(params[:search]).order("created_at DESC")
-    else
-      @orders = Order.all.order("created_at DESC")
   end
 end
