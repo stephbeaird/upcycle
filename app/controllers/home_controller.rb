1484b4638a00bc453aceb55119935ee4f4f160d2
stephbeaird <sbeaird22@gmail.com>
2018-05-02 15:43:35 -0700

    removed 'conditions' in search method in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ShopifyApp::AuthenticatedController
   end
 
   def search
-    @orders = ShopifyAPI::Order.where(:all, :conditions => { :order => 'number' })
+    @orders = ShopifyAPI::Order.where(:conditions => 'number')
   end
 end
