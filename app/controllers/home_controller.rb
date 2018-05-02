aea5bf29887b3842198d75b063de9ec1aab215fc
stephbeaird <sbeaird22@gmail.com>
2018-05-02 15:39:40 -0700

    edited search method in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ShopifyApp::AuthenticatedController
   end
 
   def search
-    @orders = ShopifyAPI::Order.find(:all, params[:number])
+    @orders = ShopifyAPI::Order.where(:all, :conditions => { :order.number => 'number' })
   end
 end
