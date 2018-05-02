71d1616ee8ad0fb3b5bdeb1cdbbec6498f209311
stephbeaird <sbeaird22@gmail.com>
2018-05-02 10:59:42 -0700

    w

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    if params[:search]
-      @orders = ShopifyAPI::Order.search(:all, params[:number])
-    else
-      @orders = ShopifyAPI::Order.find(:all, parmas: { limit: 25 })
-    end
+    @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
   end
 
   def search
