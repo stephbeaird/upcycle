485370cd536ab6cbcf779072b9cf6f276bba534c
stephbeaird <sbeaird22@gmail.com>
2018-05-02 10:56:44 -0700

    fsadfsa

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
-  "https://github.com/#{repo_name}.git"
+  "https://github.com/upcycle.git"
 end
 
 gem 'shopify_app'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
+    if params[:search]
+      @orders = ShopifyAPI::Order.search(:all, params[:number])
+    else
+      @orders = ShopifyAPI::Order.find(:all, parmas: { limit: 25 })
+    end
   end
 
   def search
