77f3d189b4dbe06c2a6e716434494106e24302b9
stephbeaird <sbeaird22@gmail.com>
2018-05-03 15:49:21 -0700

    revert back to number in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ShopifyApp::AuthenticatedController
   end
 
   def search
-    @orders = ShopifyAPI::Order.where(params[:search]).number
+    @orders = ShopifyAPI::Order.where(type: "number" )
   end
 end
