73a44b1a7bef381d37c1133514a6076da3adc97b
stephbeaird <sbeaird22@gmail.com>
2018-05-03 13:24:01 -0700

    changed search controller to original .where

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ShopifyApp::AuthenticatedController
   end
 
   def search
-    @orders = ShopifyAPI::Order.find(params[:number])
+    @orders = ShopifyAPI::Order.where(type: "number" )
   end
 end
