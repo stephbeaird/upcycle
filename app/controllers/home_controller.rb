739cb2bff14be0a1f3c24bfd7c53cb51a7166779
stephbeaird <sbeaird22@gmail.com>
2018-05-04 10:31:48 -0700

    revert back to original in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ShopifyApp::AuthenticatedController
   end
 
   def search
-    @orders = ShopifyAPI::Order.where('number LIKE ?', "%#{params[:search]%")
+    @orders = ShopifyAPI::Order.where(type: "number" )
   end
 end
