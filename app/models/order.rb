fa0f8f97bf8de96bbba2f77fec4e360343720fa2
stephbeaird <sbeaird22@gmail.com>
2018-05-04 15:23:42 -0700

    code in models

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,2 @@
 class Order < ApplicationRecord
-  def self.search(orders)
-    if orders
-      where("number LIKE ?", "%#{orders}%")
-    end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,3 +1,8 @@
 class Search < ActiveRecord::Base
   include ShopifyApp::SessionStorage
+  def self.search(orders)
+    if orders
+      where("number LIKE ?", "%#{orders}%")
+    end
+  end
 end
