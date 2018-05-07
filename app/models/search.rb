50ab1a7288f921aac907826da0e6e4aac87d7a2d
stephbeaird <sbeaird22@gmail.com>
2018-05-07 12:16:03 -0700

    reverse to original

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,8 +1,3 @@
 class Search < ActiveRecord::Base
   include ShopifyApp::SessionStorage
-  def self.search(search, number)
-    paginate :per_number => 1, :number => number,
-             :conditions =>['number like ?', "%#{search}%"]
-             :order => 'number'
-  end
 end
