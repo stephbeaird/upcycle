49f430b49c592949f0218e485b0959c721247b00
stephbeaird <sbeaird22@gmail.com>
2018-05-08 15:00:23 -0700

    added code to search method in home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,12 @@ class HomeController < ShopifyApp::AuthenticatedController
   end
 
   def search
+    limit = params[:limit] || 10
+    page = params[:page] || 10
+    offset = ((page.to_i -1) * limit.to_i) || 0
     term = params[:term] || nil
     orders = [:number, :first_name, :last_name]
-    @orders = ShopifyAPI::Order.where(type: "number")
+    @orders = ShopifyAPI::Order.where('number LIKE ?', "%{term}%").limit(limit).offset(offset)if term
     render json: orders
   end
 end
