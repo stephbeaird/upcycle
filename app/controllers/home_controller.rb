5c717069c2b1e917a5d9b0017f0785c2841ccdbb
stephbeaird <sbeaird22@gmail.com>
2018-05-08 10:52:02 -0700

    removed validation in home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,4 @@
 class HomeController < ShopifyApp::AuthenticatedController
-    validates :number, presence: true
-  end
-
-  Order.create(number: nil).valid? # => false
-
   def index
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
   end
