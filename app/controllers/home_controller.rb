bdb754ef9c8f8a6677361f5601803b1a22a65300
stephbeaird <sbeaird22@gmail.com>
2018-05-08 11:52:49 -0700

    removed session from index method in home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
-    @orders.session[:shopify] = order.number
   end
 
   def search
