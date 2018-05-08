26336aa7c67e9c1b322bd5d1a5601e8a0f4fa121
stephbeaird <sbeaird22@gmail.com>
2018-05-08 15:08:23 -0700

    reverse

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,6 @@ class HomeController < ShopifyApp::AuthenticatedController
   end
 
   def search
-    limit = params[:limit] || 10
-    page = params[:page] || 10
-    offset = ((page.to_i -1) * limit.to_i) || 0
     term = params[:term] || nil
     orders = [:number, :first_name, :last_name]
     @orders = ShopifyAPI::Order.where(type: "number")
