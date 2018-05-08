acadcfbc6328f4ef932fa5df7e0a28c417b288d9
stephbeaird <sbeaird22@gmail.com>
2018-05-08 13:22:59 -0700

    removed params from index method in  home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
+    @orders = ShopifyAPI::Order.find(:all)
   end
 
   def search
