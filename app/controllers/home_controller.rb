caf1dd762b24781cd9934e0cd48e9b3b03716af0
stephbeaird <sbeaird22@gmail.com>
2018-05-08 13:40:17 -0700

    adjusted limit in index method from 25 to 50

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
+    @orders = ShopifyAPI::Order.find(:all, params: { limit: 50 })
   end
 
   def search
