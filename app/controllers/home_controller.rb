861f27b74349f34910d4d1d9609d00114c9886f3
stephbeaird <sbeaird22@gmail.com>
2018-05-09 16:13:10 -0700

    change params for search method in home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ShopifyApp::AuthenticatedController
   end
 
   def search
-    @orders = ShopifyAPI::Order.where(type: "number")
+    @orders = ShopifyAPI::Order.search
   end
 end
  
