da8a93f73671b58ee89c15eac02f3bb2e6221e0a
stephbeaird <sbeaird22@gmail.com>
2018-05-09 11:46:26 -0700

    changed index method in home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @orders = ShopifyAPI::Order.find(:all, params: { limit: 50 })
+    @orders = ShopifyAPI::Order.search(params[:search])
   end
 
   def search
