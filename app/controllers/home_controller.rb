bc4878113352ed6fd599c3222d4c177e1cf37d74
stephbeaird <sbeaird22@gmail.com>
2018-05-09 12:41:01 -0700

    adjusted limit in index method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @orders = ShopifyAPI::Order.find(:all, params: {limit: 50 })
+    @orders = ShopifyAPI::Order.find(:all, params: {limit: 10 })
   end
 
   def search
