7241fa520e386b7734379b4a96b891a71fe3b203
stephbeaird <sbeaird22@gmail.com>
2018-05-09 12:44:21 -0700

    changed params for search method in home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @orders = ShopifyAPI::Order.find(:all, params: {limit: 10 })
+    @orders = ShopifyAPI::Order.find(:all, params: { limit: 10 })
   end
 
   def search
-    @orders = ShopifyAPI::Order.where(type: "number")
+    @orders = ShopifyAPI::Order.find(:all, params: { limit: 10 })
   end
 end
  
